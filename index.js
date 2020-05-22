1d60b0bed745e975ca72d858495f20a8470b6d5c
Swapnil Patil <swapnilpatil@Swapnils-MacBook-Pro-2.local>
2020-05-21 20:22:56 -0700

    boxplot

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var port = process.env.PORT || 3000
 var express = require('express')
 var path = require('path')
 var app = express()
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/app'))
 
 app.get('/', function(req, res) {
     var options = {
-        root: path.join(__dirname, "app/html")
+        root: path.join(__dirname, "app")
     }
     res.sendFile('dashboard.html', options)
 })
